dfbedcbb29b21648234b0153326a40dedb2c9bbf
Weiwei <linweiwei2222@163.com>
2023-11-06 20:15:15 -0500

    practice

--- React_Week1_Day1/src/App.jsx
+++ React_Week1_Day1/src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Dashboard from './components/Dashboard'
-import Reviews from './components/Reviews'
-import Rating from './components/Rating'
-import Analysis from './components/Analysis'
-import Visitors from './components/Visitors'
-import './style.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Reviews from "./components/Reviews";
+import Rating from "./components/Rating";
+import Analysis from "./components/Analysis";
+import Visitors from "./components/Visitors";
+import "./style.css";
 
 function App() {
-
   return (
-      <div className="grid-container">
-        <Dashboard />
-        <Reviews />
-        <Rating />
-        <Analysis />
-        <Visitors />
-      </div>
-  )
+    <div className="grid-container">
+      <Dashboard />
+      <Reviews />
+      <Rating />
+      <Analysis />
+      <Visitors />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- React_Week1_Day1/src/components/Rating.jsx
+++ React_Week1_Day1/src/components/Rating.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Rating() {
-    return (
-        <div className="rating">this is Rating
-        </div>
-    )
-  }
-  
-  export default Rating
-  
+  return <div className="rating">this is Rating</div>;
+}
+
+export default Rating;
